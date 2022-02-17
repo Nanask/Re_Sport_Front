80cf3191ab93d7e43dc2622b8d490119aeb06a8d
Nanask <ybhag1005@daum.net>
2022-02-18 01:24:01 +0900

    search, search provider 수정

--- sport_react/src/comp/search/Search.jsx
+++ sport_react/src/comp/search/Search.jsx
@@ -1,86 +1,23 @@
-import React, { useEffect } from "react";
-// import "../../css/search/search.css";
+import React, { useEffect, useState } from "react";
 import { useSearchContext } from "./../../context/SearchContext";
 import UseInput from "./../custom/UseInput";
 
 const Search = () => {
-  const { detailList, getDList } = useSearchContext();
+  const { detailList, getDList, onChangeHandler, dtList, checkBoxList, radioList } = useSearchContext();
 
   useEffect(getDList, []);
 
-  const dtList = detailList.map((list, index) => {
-    return (
-      <tr className="border-2 text-center" key={list.seq}>
-        <td className="p-5 text-center space-x-8">{index + 1}</td>
-        <td>{list.al_code}</td>
-        <td>{list.al_tel}</td>
-        <td>{list.al_sport}</td>
-        <td>{list.al_addr}</td>
-        <td>{list.al_free}</td>
-      </tr>
-    );
-  });
-
   return (
-    // <div>Search</div>
     <div className="flex flex-col justify-center items-center space-y-5 font-kr">
       <h1 className="mt-10 font-bold text-3xl">광주광역시 체육시설 찾아보기</h1>
       <div className="inline-block w-300 text-center border-2">
         <div className="flex space-x-10 p-5">
           <div className="">이용료</div>
-          {/* <div className="flex space-x-3 "> */}
-          <UseInput type="radio" name="radio" id="radio1" label="유료" />
-          <UseInput type="radio" name="radio" id="radio2" label="무료" />
-          <UseInput type="radio" name="radio" id="radio3" label="유/무료" />
-          {/* <div className="flex items-center space-x-2">
-            <input id="ra" type="radio" name="ra1" value="무료" />
-            <label>무료</label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <input id="ra" type="radio" name="ra2" value="유료" />
-            <label>유료</label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <input id="ra" type="radio" name="ra3" value="유/무료" />
-            <label>유/무료</label>
-          </div> */}
-          {/* </div> */}
+          {radioList}
         </div>
         <div className="flex space-x-10 p-5">
           <div>지역(구)</div>
-          <div className="flex space-x-3">
-            <UseInput type="checkbox" name="check1" id="check1" label="동구" />
-            <UseInput type="checkbox" name="check2" id="check2" label="서구" />
-            <UseInput type="checkbox" name="check3" id="check3" label="남구" />
-            <UseInput type="checkbox" name="check4" id="check4" label="북구" />
-            <UseInput type="checkbox" name="check5" id="check5" label="광산구" />
-            <UseInput type="checkbox" name="check6" id="check6" label="전체" />
-
-            {/* <div className="flex items-center space-x-2">
-              <input id="ch" type="checkbox" name="ch1" value="동구" />
-              <label>동구</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input id="ch" type="checkbox" name="ch2" value="서구" />
-              <label>서구</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input id="ch" type="checkbox" name="ch3" value="남구" />
-              <label>남구</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input id="ch" type="checkbox" name="ch4" value="북구" />
-              <label>북구</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input id="ch" type="checkbox" name="ch5" value="광산구" />
-              <label>광산구</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input id="all" type="checkbox" name="ch_all" value="전체" onclick="selectAll" />
-              <label>전체</label>
-            </div> */}
-          </div>
+          <div className="flex space-x-3">{checkBoxList}</div>
         </div>
         <div className="flex space-x-10 p-5">
           <div>지역(동)</div>
@@ -88,17 +25,14 @@ const Search = () => {
             <input className="border-2" type="text" name="dong" />
           </div>
         </div>
-        {/* <!-- 드롭다운 사용하기 --> */}
         <div className="flex space-x-10 p-5">
           <div className="m-30">종목</div>
-          {/* <div className="flex space-x-3"> */}
           <select className="border-2">
             <option value="1">--- 종목 ---</option>
             <option value="2">배드민턴</option>
             <option value="3">3</option>
             <option value="4">4</option>
           </select>
-          {/* </div> */}
         </div>
         <div>
           <button type="button" className="pt-2 pr-3 pl-3 pb-2 hover:bg-slate-200 m-3">
@@ -107,7 +41,6 @@ const Search = () => {
         </div>
       </div>
       <table>
-        {/* 배열 데이터 만들어서 map 돌려서 출력하기 !! 제발제발제발제발제발제발 */}
         <tr className="border-2 text-center">
           <th className="p-4">번호</th>
           <th>시설 이름</th>
--- sport_react/src/context/SearchContext.jsx
+++ sport_react/src/context/SearchContext.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { createContext, useContext } from "react";
+import UseInput from "./../comp/custom/UseInput";
 
 const AppContext = createContext();
 
@@ -45,6 +46,110 @@ const SearchContext = ({ children }) => {
     },
   ]);
 
+  const onChangeHandler = (e) => {
+    // const { name, value } = e.target;
+    const { checked, value, name } = e.target;
+    console.log("value", value);
+    console.log("checkId", checked);
+    // console.log("dddddd");
+    // }
+    // if (type === "checkbox") {
+    //   if (checked) {
+    //     setCheckedInput([...checkedInput, name]);
+    //     console.log(checkedInput);
+    //   } else {
+    //     setCheckedInput(checkedInput.filter((check) => check !== name));
+    //     console.log(checkedInput);
+    // }
+    // }
+    // console.log("라라라라라라");
+  };
+
+  // radio List 만들기
+  const radioItems = [
+    {
+      type: "radio",
+      name: "radio",
+      value: "pay",
+      label: "유료",
+    },
+    {
+      type: "radio",
+      name: "radio",
+      value: "free",
+      label: "무료",
+    },
+    {
+      type: "radio",
+      name: "radio",
+      value: "payFree",
+      label: "유/무료",
+    },
+  ];
+
+  const radioList = radioItems.map((item) => {
+    return <UseInput type={item.type} name={item.name} value={item.value} label={item.label} propChange={onChangeHandler}></UseInput>;
+  });
+
+  // checkBox List 만들기
+  const checkBoxItems = [
+    {
+      type: "checkbox",
+      name: "check1",
+      id: "check1",
+      label: "동구",
+    },
+    {
+      type: "checkbox",
+      name: "check2",
+      id: "check2",
+      label: "서구",
+    },
+    {
+      type: "checkbox",
+      name: "check3",
+      id: "check3",
+      label: "남구",
+    },
+    {
+      type: "checkbox",
+      name: "check4",
+      id: "check4",
+      label: "북구",
+    },
+    {
+      type: "checkbox",
+      name: "check5",
+      id: "check5",
+      label: "광산구",
+    },
+    {
+      type: "checkbox",
+      name: "check6",
+      id: "check6",
+      label: "전체",
+    },
+  ];
+
+  const checkBoxList = checkBoxItems.map((item) => {
+    return <UseInput type={item.type} name={item.name} id={item.id} label={item.label} propChange={onChangeHandler}></UseInput>;
+  });
+
+  const dtList = detailList.map((list, index) => {
+    return (
+      <tr className="border-2 text-center" key={list.seq}>
+        <td className="p-5 text-center space-x-8">{index + 1}</td>
+        <td>{list.al_code}</td>
+        <td>{list.al_tel}</td>
+        <td>{list.al_sport}</td>
+        <td>{list.al_addr}</td>
+        <td>{list.al_free}</td>
+      </tr>
+    );
+  });
+
+  const [checkedInput, setCheckedInput] = useState([]);
+
   const getDList = async () => {
     const res = await fetch("http://localhost:8080/search/");
     const result = await res.json();
@@ -58,11 +163,7 @@ const SearchContext = ({ children }) => {
     setDetailList(resultArray);
   };
 
-  // const dtList = GetDList.forEach((item) => {
-  //   console.log(item);
-  // });
-
-  const props = { detailList, getDList };
+  const props = { detailList, getDList, onChangeHandler, dtList, checkBoxList, radioList };
   return <AppContext.Provider value={props}>{children}</AppContext.Provider>;
 };
 
